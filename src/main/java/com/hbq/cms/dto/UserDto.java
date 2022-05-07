e005942270dc81608b318ea507778200030f0d66
hui <huibq@cheche365.com>
2022-05-07 17:08:16 +0800

    用户模块支持验证码登录注册和修改密码

--- src/main/java/com/hbq/cms/dto/UserDto.java
+++ src/main/java/com/hbq/cms/dto/UserDto.java
@@ -1,10 +1,11 @@
 package com.hbq.cms.dto;
 
+import com.hbq.cms.model.User;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 
 /**
@@ -15,17 +16,14 @@ import javax.validation.constraints.NotEmpty;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class UserDto {
+public class UserDto extends User {
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "账号（用户名）")
-    @NotEmpty(message = "账号不能为空")
-    private String account;
-    @ApiModelProperty(value = "用户名")
-    private String name;
-    @NotEmpty(message = "请输入密码")
-    @ApiModelProperty(value = "密码")
-    private String pwd;
     @ApiModelProperty(value = "修改密码")
     private String newPwd;
+    @ApiModelProperty(value = "验证码")
+    private String code;
+    @NotNull(message = "类型不能为空")
+    @ApiModelProperty(value = "验证码类型")
+    private Integer codeType;
 }
